25a99f0098558cb472f63150000fe32a33f06239
Amit Rai <arajanrai@gmail.com>
2024-03-24 17:37:33 +0530

    Widget complete till counter multiple size

--- app/src/main/java/com/onemb/onembwidgets/widgets/screenunlock/ScreenUnlockCounterWidget.kt
+++ app/src/main/java/com/onemb/onembwidgets/widgets/screenunlock/ScreenUnlockCounterWidget.kt
@@ -7,7 +7,17 @@ package com.onemb.onembwidgets.widgets.screenunlock
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.res.Resources
+import android.hardware.display.DisplayManager
+import android.os.Build
+import android.util.DisplayMetrics
 import android.util.Log
+import android.util.Size
+import android.view.Display
+import android.view.WindowManager
+import android.view.WindowMetrics
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.TextUnit
@@ -18,6 +28,7 @@ import androidx.core.content.ContextCompat.registerReceiver
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.LocalSize
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.CircularProgressIndicator
 import androidx.glance.appwidget.GlanceAppWidget
@@ -43,22 +54,32 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.Calendar
 
+
 /**
  * ScreenUnlockCounterWidget is a GlanceAppWidget that displays the screen unlock counter.
  * It updates the counter on screen unlock events and provides a Glance theme for widget appearance.
  */
 class ScreenUnlockCounterWidget: GlanceAppWidget() {
 
-    companion object {
-        private val thinMode = DpSize(120.dp, 120.dp)
-        private val smallMode = DpSize(184.dp, 184.dp)
-        private val mediumMode = DpSize(260.dp, 200.dp)
-        private val largeMode = DpSize(260.dp, 280.dp)
+    override val sizeMode = SizeMode.Exact
+
+
+    fun screenSize(context: Context): Pair<Int, Int> {
+        val displayManager = context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        val defaultDisplay = displayManager.getDisplay(Display.DEFAULT_DISPLAY)
+        val defaultDisplayContext = context.createDisplayContext(defaultDisplay)
+
+        val density = defaultDisplayContext.resources.displayMetrics.density
+        val widthDp = Math.round(defaultDisplayContext.resources.displayMetrics.widthPixels / density)
+        val heightDp = Math.round(defaultDisplayContext.resources.displayMetrics.heightPixels / density)
+        return Pair(widthDp, heightDp)
     }
 
-    override val sizeMode: SizeMode = SizeMode.Responsive(
-        setOf(thinMode, smallMode, mediumMode, largeMode)
-    )
+
+
+//    override val sizeMode: SizeMode = SizeMode.Responsive(
+//        setOf(thinMode, smallMode, mediumMode, largeMode)
+//    )
 
 
     override val stateDefinition = ScreenUnlockStateDefinition
@@ -75,114 +96,34 @@ class ScreenUnlockCounterWidget: GlanceAppWidget() {
         val dayOfWeek = weekArray[calendarIndex]
         provideContent {
             val screenUnlockState = currentState<ScreenUnlockState>()
-            Log.d("STATE", screenUnlockState.toString())
+            val size = LocalSize.current
+//            Log.d("SIZE", size.toString())
+            Log.d("SIZE", size.width.toString())
+
             GlanceTheme {
                 when (screenUnlockState) {
                     ScreenUnlockState.Loading -> {
                         CircularProgressIndicator()
                     }
                     is ScreenUnlockState.Available -> {
-                        Log.d("TEST", screenUnlockState.currentData.counter.toString())
-                        Column(
-                            modifier = GlanceModifier.fillMaxSize().background(Color.White)
-                                .clickable {
-                                    val sharedPreferences = context.getSharedPreferences("ACTION_USER_PRESENT", Context.MODE_PRIVATE)
-                                    val registered = sharedPreferences.getBoolean("registered", false)
-                                    val filter = IntentFilter(Intent.ACTION_USER_PRESENT)
-                                    if (!registered) {
-                                        val editor = sharedPreferences.edit()
-                                        editor.putBoolean("registered", true)
-                                        editor.apply()
-                                        registerReceiver(
-                                            context, receiver, filter,
-                                            ContextCompat.RECEIVER_VISIBLE_TO_INSTANT_APPS
-                                        )
-                                    } else {
-                                        try {
-                                            context.unregisterReceiver(receiver)
-                                        } catch (e:Exception) {
-                                            Log.e("Error", e.message.toString())
-                                        }
-                                        CoroutineScope(Dispatchers.IO).launch {
-                                            delay(2000)
-                                            try {
-                                                registerReceiver(
-                                                    context, receiver, filter,
-                                                    ContextCompat.RECEIVER_VISIBLE_TO_INSTANT_APPS
-                                                )
-                                            } catch (e:Exception) {
-                                                Log.e("Error", e.message.toString())
-                                            }
-                                        }
-                                    }
-                                    val workRequest = OneTimeWorkRequestBuilder<ScreenUnlockWorker>()
-                                        .build()
-                                    WorkManager.getInstance(context).enqueue(workRequest)
-                                },
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Column(modifier = GlanceModifier.padding(all = 16.dp).fillMaxSize()) {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    modifier = GlanceModifier.fillMaxWidth()
-                                ) {
-                                    val modifier = GlanceModifier.defaultWeight()
-                                    for (item in weekArray) {
-                                        if (item == dayOfWeek) {
-                                            val modifierActive = GlanceModifier.defaultWeight()
-                                            Box(
-                                                contentAlignment = Alignment.Center,
-                                                modifier = modifierActive,
-                                            ) {
-                                                Text(
-                                                    text = item,
-                                                    style = TextStyle(
-                                                        color = ColorProvider(Color.Red)
-                                                    )
-                                                )
-                                            }
-                                        } else {
-                                            Box(
-                                                contentAlignment = Alignment.Center,
-                                                modifier = modifier
-                                            ) {
-                                                Text(text = item)
-                                            }
-                                        }
-                                    }
-                                }
-                                Row(
-                                    verticalAlignment = Alignment.Bottom,
-                                    horizontalAlignment = Alignment.Start,
-                                    modifier = GlanceModifier.fillMaxSize().padding(start = 10.dp)
-                                ) {
-                                    Box(
-                                        contentAlignment = Alignment.CenterStart
-                                    ) {
-                                        Text(
-                                            text = "Screen Unlocks: ",
-                                            style = TextStyle(
-                                                color = ColorProvider(Color.Black),
-                                                fontSize = TextUnit(24F, TextUnitType.Sp)
-                                            )
-                                        )
-                                    }
-                                    Box(
-                                        contentAlignment = Alignment.CenterEnd
-                                    ) {
-                                        Text(
-                                            text = "${screenUnlockState.currentData.counter}",
-                                            style = TextStyle(
-                                                color = ColorProvider(Color.Red),
-                                                fontSize = TextUnit(24F, TextUnitType.Sp)
-                                            )
-                                        )
-                                    }
-                                }
-
-                            }
+                        if(size.width > 450.dp) {
+                            ScreenUnlockCounterLarge(
+                                context,
+                                receiver,
+                                weekArray,
+                                dayOfWeek,
+                                screenUnlockState.currentData.counter
+                            )
+                        } else if(size.width > 281.dp && size.width <= 450.dp) {
+                            ScreenUnlockCounterMedium(
+                                weekArray,
+                                dayOfWeek,
+                                screenUnlockState.currentData.counter
+                            )
+                        } else if(size.width < 281.dp) {
+                            ScreenUnlockCounterSmall(
+                                screenUnlockState.currentData.counter
+                            )
                         }
                     }
 
@@ -192,6 +133,212 @@ class ScreenUnlockCounterWidget: GlanceAppWidget() {
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun ScreenUnlockCounterLarge(
+    context: Context,
+    receiver: ScreenUnlockCounterReceiver,
+    weekArray: Array<String>,
+    dayOfWeek: String,
+    screenUnlockState: Int
+) {
+    Column(
+        modifier = GlanceModifier.fillMaxSize().background(Color.Transparent)
+            .clickable {
+                val sharedPreferences = context.getSharedPreferences("ACTION_USER_PRESENT", Context.MODE_PRIVATE)
+                val registered = sharedPreferences.getBoolean("registered", false)
+                val filter = IntentFilter(Intent.ACTION_USER_PRESENT)
+                if (!registered) {
+                    val editor = sharedPreferences.edit()
+                    editor.putBoolean("registered", true)
+                    editor.apply()
+                    registerReceiver(
+                        context, receiver, filter,
+                        ContextCompat.RECEIVER_VISIBLE_TO_INSTANT_APPS
+                    )
+                } else {
+                    try {
+                        context.unregisterReceiver(receiver)
+                    } catch (e:Exception) {
+                        Log.e("Error", e.message.toString())
+                    }
+                    CoroutineScope(Dispatchers.IO).launch {
+                        delay(2000)
+                        try {
+                            registerReceiver(
+                                context, receiver, filter,
+                                ContextCompat.RECEIVER_VISIBLE_TO_INSTANT_APPS
+                            )
+                        } catch (e:Exception) {
+                            Log.e("Error", e.message.toString())
+                        }
+                    }
+                }
+                val workRequest = OneTimeWorkRequestBuilder<ScreenUnlockWorker>()
+                    .build()
+                WorkManager.getInstance(context).enqueue(workRequest)
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(modifier = GlanceModifier.padding(all = 16.dp).fillMaxSize()) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = GlanceModifier.fillMaxWidth()
+            ) {
+                val modifier = GlanceModifier.defaultWeight()
+                for (item in weekArray) {
+                    if (item == dayOfWeek) {
+                        val modifierActive = GlanceModifier.defaultWeight()
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = modifierActive,
+                        ) {
+                            Text(
+                                text = item,
+                                style = TextStyle(
+                                    color = ColorProvider(Color.Red)
+                                )
+                            )
+                        }
+                    } else {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = modifier
+                        ) {
+                            Text(
+                                text = item,
+                                style = TextStyle(
+                                    color = ColorProvider(Color.White)
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalAlignment = Alignment.Start,
+                modifier = GlanceModifier.fillMaxSize().padding(start = 10.dp)
+            ) {
+                Box(
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    Text(
+                        text = "Screen Unlocks: ",
+                        style = TextStyle(
+                            color = ColorProvider(Color.White),
+                            fontSize = TextUnit(24F, TextUnitType.Sp)
+                        )
+                    )
+                }
+                Box(
+                    contentAlignment = Alignment.CenterEnd
+                ) {
+                    Text(
+                        text = "$screenUnlockState",
+                        style = TextStyle(
+                            color = ColorProvider(Color.Red),
+                            fontSize = TextUnit(24F, TextUnitType.Sp)
+                        )
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ScreenUnlockCounterMedium(
+    weekArray: Array<String>,
+    dayOfWeek: String,
+    screenUnlockState: Int
+) {
+    Column(modifier = GlanceModifier.padding(all = 16.dp).fillMaxSize()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = GlanceModifier.fillMaxWidth()
+        ) {
+            val modifier = GlanceModifier.defaultWeight()
+            for (item in weekArray) {
+                if (item == dayOfWeek) {
+                    val modifierActive = GlanceModifier.defaultWeight()
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = modifierActive,
+                    ) {
+                        Text(
+                            text = item,
+                            style = TextStyle(
+                                color = ColorProvider(Color.Red)
+                            )
+                        )
+                    }
+                } else {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = modifier
+                    ) {
+                        Text(
+                            text = item,
+                            style = TextStyle(
+                                color = ColorProvider(Color.White)
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = GlanceModifier.fillMaxSize()
+        ) {
+
+            Box(
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = "$screenUnlockState",
+                    style = TextStyle(
+                        color = ColorProvider(Color.Red),
+                        fontSize = TextUnit(55F, TextUnitType.Sp)
+                    )
+                )
+            }
+        }
+
+    }
+}
+@Composable
+fun ScreenUnlockCounterSmall(
+    screenUnlockState: Int
+) {
+    Column(modifier = GlanceModifier.fillMaxSize()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = GlanceModifier.fillMaxSize()
+        ) {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "$screenUnlockState",
+                    style = TextStyle(
+                        color = ColorProvider(Color.Red),
+                        fontSize = TextUnit(48F, TextUnitType.Sp)
+                    )
+                )
+            }
+        }
+
     }
 }
 
